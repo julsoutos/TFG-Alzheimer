62ac7dda314713350c5ab3d1559e6d21d84951c5
Pablo Rodriguez Garrido <pabrodgar9@alum.us.es>
2021-04-23 11:54:57 +0200

    Configuración inicial css
    Se ha reconfigurado la dirección de static para cargar todos los ficheros y datos referidos al aspecto de la aplicación.
    
    Tarea relacionada: #15
    
    Co-Authored-By: Julia Soult Toscano <56022076+julsoutos@users.noreply.github.com>

--- cognitya/settings.py
+++ cognitya/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'principal'
 ]
 
 
@@ -136,6 +138,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 try:
     from local_settings import *
